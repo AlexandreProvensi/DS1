c6ff922b9eaafd31619bdeca045e37fe856721eb
AlexandreProvensi <alexandreprovensi12@gmail.com>
2020-05-28 19:16:09 -0300

    Aula 22-05-2020

--- app-angular/app/controller/cliente.controller.js
+++ app-angular/app/controller/cliente.controller.js
@@ -1,20 +1,77 @@
-(function(app) {
-    'use strict';
+(function( app ){
+   'use strict';
+   
+   app.controller('ClienteController', function( $scope, ClienteService ){
+    //Controle para OrderBy e Filter
+    $scope.decrescente = false;
+    $scope.selectedColumn = 'id';
 
-    app.controller('ClienteController', function($scope, ClienteService) {
+    //Controle de exibição da tabela/formulario
+    $scope.showTable = true;
 
-        //Carrega uma lista de clientes
-        ClienteService.listar().then(function(result) {
-            $scope.clientes = result.data;
-        }).catch(function(error) {
-            if (error.status == 404) {
-                $scope.msgerro = 'O recurso não foi encontrado';
-            } else {
-                console.log(error);
-            }
+    
 
+    //Seta a coluna para ser filtrada/ordenada
+    $scope.setColumn = function ( columnName ){
+        $scope.selectedColumn = columnName;
+
+        //determina o ordenação decrescente (false)
+        $scope.decrescente = !$scope.decrescente;
+    }
+
+    //Retornar para o FILTER qual a coluna será utilizada na ordenação/filtro
+    $scope.filter = function() {
+        var filtro = {};
+
+        filtro[$scope.selectedColumn] = $scope.textFilter;
+
+        return filtro;
+    }
+
+    //Prepara a tela para um novo cadastro
+    $scope.novo = function() {
+        //Representar o cliente atual
+        $scope.cliente = {
+            nome: '',
+            email: '',
+            cidade: '',
+            estado: ''
+        }
+
+        $scope.showTable = false;
+    }
+
+    //Cancelar a inclusao/edicao
+    $scope.cancelar = function () {
+        $scope.showTable = true;
+    }
+
+    //Salvar a inclusão/edição do cliente
+    $scope.salvar = function() {
+        ClienteService.salvar($scope.cliente).then(function( result) {
+            $scope.showTable = true;
         });
+        
+    }
 
+    //Editar o cliente selecionado
+    $scope.editar = function(cliente) {
+        $scope.cliente = cliente;
+        $scope.showTable = false;
+    }
+
+    //Excluir o cliente selecionado
+    $scope.excluir = function() {
+        ClienteService.remover($scope.cliente).then(function(result){
+            $scope.showTable = true;
+        });
+    }
+
+    //Carrega uma lista de clientes
+    ClienteService.listar().then(function( result ){
+        $scope.clientes = result.data;
     });
 
-})(appJS);
+   });
+
+})( appJS );
