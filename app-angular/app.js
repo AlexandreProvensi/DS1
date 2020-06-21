7b4edc3f997966254fcffb10bd25417ecc5948e6
AlexandreProvensi <alexandreprovensi12@gmail.com>
2020-06-21 17:05:36 -0300

    Aula 04/06/2020 - Atrasada hehe

--- app-angular/app.js
+++ app-angular/app.js
@@ -12,5 +12,28 @@ appJS.config(function($routeProvider) {
             templateUrl: 'app/view/estado.view.html',
             controller: 'EstadoController'
         })
+        .when('/signin', {
+            templateUrl: 'app/view/signin.view.html',
+            controller: 'SignController'
+        })
+        .when('/signup', {
+            templateUrl: 'app/view/signup.view.html',
+            controller: 'SignController'
+        })
         .otherwise({ redirectTo: '/clientes' });
+});
+
+//Defino o que acontecerá na execução da aplicação
+appJS.run(function($rootScope, $location, $sessionStorage) {
+
+    $rootScope.$on('$locationChangeStart', function() {
+
+        if ($location.path().indexOf('sign') < 0) {
+            //Verifica se o usuário entrou
+            if (!$sessionStorage.logado) {
+                $location.path('/signin');
+            }
+        }
+    });
+
 });
